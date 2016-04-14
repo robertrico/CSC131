0bace6d826292ce7b7a317a2fab109c220654dd0
Robert Rico <robert.m.rico@gmail.com>
2016-04-14 16:55:32 -0700

    Hours and student infos
    Yo

---
+++ app/Model/StudentInfo.php
@@ -0,0 +1,62 @@
+<?php
+App::uses('AppModel', 'Model');
+/**
+ * StudentInfo Model
+ *
+ * @property Hour $Hour
+ * @property User $User
+ */
+class StudentInfo extends AppModel {
+
+/**
+ * Validation rules
+ *
+ * @var array
+ */
+	public $validate = array(
+		'hour_id' => array(
+			'numeric' => array(
+				'rule' => array('numeric'),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
+		'user_id' => array(
+			'numeric' => array(
+				'rule' => array('numeric'),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
+	);
+
+	// The Associations below have been created with all possible keys, those that are not needed can be removed
+
+/**
+ * belongsTo associations
+ *
+ * @var array
+ */
+	public $belongsTo = array(
+		'Hour' => array(
+			'className' => 'Hour',
+			'foreignKey' => 'hour_id',
+			'conditions' => '',
+			'fields' => '',
+			'order' => ''
+		),
+		'User' => array(
+			'className' => 'User',
+			'foreignKey' => 'user_id',
+			'conditions' => '',
+			'fields' => '',
+			'order' => ''
+		)
+	);
+}
