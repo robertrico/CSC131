40116d63c81e8b396962203fe4f676297d586891
Robert Rico <robert.m.rico@gmail.com>
2016-04-21 11:34:07 -0700

    Some logic
    Fixed some logic and made variables available where needed ( I don't
    remember what I did).

--- app/Controller/EventsController.php
+++ app/Controller/EventsController.php
@@ -91,6 +91,8 @@ class EventsController extends AppController {
 			throw new NotFoundException(__('Invalid event'));
 		}
 		if ($this->request->is(array('post', 'put'))) {
+			$this->request->data['Event']['time'] = date("Y-m-d H:i:s",strtotime($this->request->data['Event']['time']));
+			$this->Event->id = $id;
 			if ($this->Event->save($this->request->data)) {
 				$this->Flash->success(__('The event has been saved.'));
 				return $this->redirect(array('action' => 'index'));
